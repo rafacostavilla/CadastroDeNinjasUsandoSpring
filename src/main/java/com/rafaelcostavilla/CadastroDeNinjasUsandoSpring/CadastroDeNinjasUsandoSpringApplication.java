207e46d54f9a31693f67a3c4b0fc40a90c3241c2
rafacostavilla <rafaelcostavilla@gmail.com>
2026-04-14 12:08:14 +1200

    first commit

---
+++ src/main/java/com/rafaelcostavilla/CadastroDeNinjasUsandoSpring/CadastroDeNinjasUsandoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelcostavilla.CadastroDeNinjasUsandoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasUsandoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjasUsandoSpringApplication.class, args);
+	}
+
+}
