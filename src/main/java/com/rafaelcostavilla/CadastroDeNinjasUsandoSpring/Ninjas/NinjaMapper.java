c3bdcbfad073b568b7fda4654e2d41cb56799784
rafacostavilla <rafaelcostavilla@gmail.com>
2026-04-28 17:08:07 +1200

    refactor: Implement NinjaDTO and NinjaMapper to be used as an additional abstraction layer to be used between the Ninja Controller and the data entity

--- src/main/java/com/rafaelcostavilla/CadastroDeNinjasUsandoSpring/Ninjas/NinjaMapper.java
+++ src/main/java/com/rafaelcostavilla/CadastroDeNinjasUsandoSpring/Ninjas/NinjaMapper.java
@@ -1,4 +1,32 @@
 package com.rafaelcostavilla.CadastroDeNinjasUsandoSpring.Ninjas;
 
+import com.rafaelcostavilla.CadastroDeNinjasUsandoSpring.Ninjas.Model.NinjaEntity;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaEntity map(NinjaDTO ninjaDTO) {
+        NinjaEntity ninjaEntity = new NinjaEntity();
+        ninjaEntity.setId(ninjaDTO.getId());
+        ninjaEntity.setNome(ninjaDTO.getNome());
+        ninjaEntity.setEmail(ninjaDTO.getEmail());
+        ninjaEntity.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaEntity.setIdade(ninjaDTO.getIdade());
+        ninjaEntity.setMissao(ninjaDTO.getMissao());
+        ninjaEntity.setRanking(ninjaDTO.getRanking());
+        return ninjaEntity;
+    }
+
+    public NinjaDTO map(NinjaEntity ninjaEntity) {
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaEntity.getId());
+        ninjaDTO.setNome(ninjaEntity.getNome());
+        ninjaDTO.setEmail(ninjaEntity.getEmail());
+        ninjaDTO.setImgUrl(ninjaEntity.getImgUrl());
+        ninjaDTO.setIdade(ninjaEntity.getIdade());
+        ninjaDTO.setMissao(ninjaEntity.getMissao());
+        ninjaDTO.setRanking(ninjaEntity.getRanking());
+        return ninjaDTO;
+    }
 }
